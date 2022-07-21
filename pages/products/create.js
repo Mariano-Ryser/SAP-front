e128a6e116b7a81dfd7b4fece11d08385ad87b56
Mariano Ryser <rysermariano@gmail.com>
2022-07-21 17:07:48 +0200

    test

---
+++ pages/products/create.js
@@ -0,0 +1,80 @@
+
+import { useState } from 'react'
+function Create() {
+ 
+    const initialState = {name:'',price:0}
+    const [product, setProduct] = useState(initialState)
+
+    const handleChange = (e) =>{
+        const inputValue = e.target.value
+        const inputName = e.target.name
+        setProduct({
+            ...product,
+            [inputName]: inputValue,
+        })
+    }
+    const handleClick = (e)=> {
+        e.preventDefault()
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`,{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(product)
+        })
+        .then(res => res.json())
+        .then((data) =>{
+            setProduct(initialState)
+            console.log("Producto creado con exito!")
+        })
+        .catch( err=> {
+            console.log("Aqui algo anda mal")
+            console.log({err})
+        })
+    }
+    
+  return (
+    <>
+        <div>
+            <h1>Nuevo producto para mi amigo MongoDB =D</h1>
+            <form>
+                <input
+                 type='text'
+                  name='name'
+                  value={product.name}
+                  onChange={handleChange}
+                  ></input>
+                <input
+                 type='number'
+                 name='price'
+                 value={product.price}
+                 onChange={handleChange}
+                 ></input>
+                <button
+                onClick={handleClick}
+                >Crear producto</button>
+            </form>
+        </div>
+        <style jsx>
+            {`
+                form{
+                    display:flex;
+                    flex-direction:column;
+                    width:20rem;
+                    margin: 0 auto
+                }
+                input{
+                    margin-bottom:0.5rem;
+                }
+
+                h1{
+                    text-align:center;
+                    background-color:rgb(241, 241, 241);
+                }
+                `}
+        </style>
+    </>
+  )
+}
+
+export default Create

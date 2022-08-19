fdca8d14094e3462bbab150fc0d7f79cf72ee622
Mariano Ryser <rysermariano@gmail.com>
2022-08-19 19:54:05 +0200

    cambiasos images, articles, and more

--- pages/code.js
+++ pages/code.js
@@ -6,8 +6,11 @@ export default function code() {
   return (
     <>
     <div className={style.contain}>  
+    <h1 className={style.h1}>Star </h1>
+    <h2 className={style.h2}>We start by creating a test function with a simple Hello World console response.</h2>
+
+          {/* BOX 1 */}
         <code className={style.boxCode}>
-            {/* <span className={style.tags}>&#8249;div&#8250;</span><span className={style.tags}>&#8249;div&#8250;</span> */}
             <span className={style.fun}>function </span>
             <span className={style.nameFun}>cataFun</span>
             <span className={style.paren}>&#40;</span>
@@ -23,22 +26,21 @@ export default function code() {
             <span className={style.brace}>&#125;</span>
         </code>
 
+          {/* BOX 2 */}
         <code className={style.boxCode}>
-            <span className={style.tags}>&#8249;div&#8250;</span> <br></br>
-            <span className={style.tags}>&#8249;div&#8250;</span> <br></br>
-            <span className={style.fun}>function </span> <br></br>
-            <span className={style.nameFun}>cataFun</span> <br></br>
-            <span className={style.paren}>&#40;</span> <br></br>
-            <span className={style.parenV}>&#40;</span> <br></br>
-            <span className={style.parenV}>&#41;</span> <br></br>
-            <span className={style.paren}>&#41;</span> <br></br>
-
-            <span className={style.cor}>&#93;</span> <br></br>
-            <span className={style.cor}>&#91;</span> <br></br>
-            <span className={style.brace}>&#123;</span> <br></br>
-            <span className={style.braceV}>&#123;</span> <br></br>
-            <span className={style.braceV}>&#125;</span> <br></br>
-            <span className={style.brace}>&#125;</span> <br></br>
+            <span className={style.tags}>&#8249;div&#8250;</span> <br></br> {/*---- < -----*/}
+            <span className={style.fun}>function </span> <br></br> {/*----- function ------*/}
+            <span className={style.nameFun}>cataFun</span> <br></br> {/*-------- function Name ------*/} <br></br>
+            <span className={style.paren}>&#40;</span> <br></br> {/*----- ( amarillo------*/}
+            <span className={style.paren}>&#41;</span> <br></br> {/*------ ) amarillo------*/}
+            <span className={style.parenV}>&#40;</span> <br></br> {/*----- ( violeta -----*/}
+            <span className={style.parenV}>&#41;</span> <br></br> {/*----- ) violeta -----*/}
+            <span className={style.cor}>&#93;</span> <br></br> {/*----- ] violeta -----*/}
+            <span className={style.cor}>&#91;</span> <br></br> {/*----- [ violeta -----*/}
+            <span className={style.brace}>&#123;</span> <br></br> {/*----- { amarillo -----*/}
+            <span className={style.brace}>&#125;</span> <br></br> {/*----- } amarillo -----*/}
+            <span className={style.braceV}>&#123;</span> <br></br> {/*----- { violeta -----*/}
+            <span className={style.braceV}>&#125;</span> <br></br> {/*----- } violeta -----*/}
         </code>
           
   

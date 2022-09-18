969d8503e2e545c9d4287811a87af31ba2f332d4
Mariano Ryser <rysermariano@gmail.com>
2022-09-18 14:37:47 +0200

    renovacion img aded

--- components/NavRespon.js
+++ components/NavRespon.js
@@ -44,6 +44,7 @@ const NavRespon = () => {
     <aside id='aside' className={styles.asideBar}>
           <ul className={styles.ul}>
             <li className={styles.li}><Link href="/"><a className={styles.a}>Home</a></Link></li>
+            <li className={styles.li}><Link href="/img"><a className={styles.a}>Images</a></Link></li>
              <li className={styles.li}><Link href="/products/create"><a className={styles.a}>Productos?</a></Link></li>
             <li className={styles.li}><Link href="/msj"><a className={styles.a}>Mensajeria</a></Link></li>
             <li className={styles.li}><Link href="https://mnr.netlify.app/"><a className={styles.a}>Icons?</a></Link></li>
@@ -52,8 +53,8 @@ const NavRespon = () => {
             
             
             <li className={styles.li}><Link href="/viventa"><a className={styles.a}>Plano?</a></Link></li>
-            <li className={styles.li}><Link href="/"><a className={styles.a}>Qrs Copiar y pegar?</a></Link></li>
-            <li className={styles.li}><Link href="/"><a className={styles.a}>...?</a></Link></li> 
+            {/* <li className={styles.li}><Link href="/"><a className={styles.a}>Qrs Copiar y pegar?</a></Link></li> */}
+            {/* <li className={styles.li}><Link href="/"><a className={styles.a}>...?</a></Link></li>  */}
             <li className={styles.li}><Link href="https://nrto.netlify.app/3d/3d.html"><a className={styles.a}>Kubik?</a></Link></li>
              <li className={styles.li}><Link href="/viventa"><a className={styles.a}>V1?</a></Link></li> 
              <li className={styles.li}><Link href="/code"><a className={styles.a}>Codes?</a></Link></li> 
---
+++ pages/img.js
@@ -0,0 +1,82 @@
+
+import Nav from '../components/NavRespon'
+import Image from 'next/image' 
+import style from '/styles/img.module.css'
+
+export default function viventa() {
+  
+  return (
+    <>
+    <div className={style.img}>
+      <Image alt='Book' src={'/fw/book.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='box' src={'/fw/bow.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+
+    <div className={style.img}>
+      <Image alt='me' src={'/fw/me.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='kirche' src={'/fw/kirche.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='himmel' src={'/fw/himmel.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/momie.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/momie2.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/mundo.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/rave.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/santuar.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/steine.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+
+    <div className={style.img}>
+      <Image alt='Burger-Icon' src={'/fw/cancha.jpg'}
+      height={1500} width={1500}
+       ></Image>
+    </div>
+    </>
+  )
+}

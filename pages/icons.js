0a07c1d1af778ba1d8ecd98836f1613c4acde43b
Mariano Ryser <rysermariano@gmail.com>
2022-09-23 17:43:44 +0200

    mas img

--- pages/icons.js
+++ pages/icons.js
@@ -18,15 +18,17 @@ const Icons = () => {
         //  style={{height: size}}
          >
 
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/node.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/insta.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/Alienverde.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/bootstrap.gif')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/bichoWhite.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/champi.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/css.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/discord.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/docker.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/cubegif.gif')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/duscord.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.docker.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/drag2.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/fo.gif')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/email.png')`}}></div>
@@ -37,40 +39,36 @@ const Icons = () => {
           <div className={styles.icon} style={{backgroundImage: `url('/img/js.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/linkedin.png')`}}></div>
           <div className={styles.icon} style={{backgroundImage: `url('/img/next.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
-          <div className={styles.icon} style={{backgroundImage: `url('/img/alien.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/github.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/googleLogo.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/paint3d.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/html.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/linux.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/window.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/watsap.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/visual.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/php.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/twiter.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/terminal.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/snake.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/tic1.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/react.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/phone.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/pizza.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/obs.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/PacYellow.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/vue.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/venus.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/venus.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/uranus.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/pluto.png')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/mooon.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/meteoro.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/mercurio2.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/marss.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/mars-unscreen.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/jupiter.gif')`}}></div>
+          <div className={styles.icon} style={{backgroundImage: `url('/img/saturn.png')`}}></div>
           
         </div>
 

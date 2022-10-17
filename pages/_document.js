09fc102546a6b0e2000e0f33f0a04fbe92201356
Mariano Ryser <rysermariano@gmail.com>
2022-10-17 13:23:49 +0200

    ? documents.js edited

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
        
       <meta charset="UTF-8"></meta>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,700&display=swap" rel="stylesheet"/>
       
       </Head>

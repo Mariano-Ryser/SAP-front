df1ae39eb787586165878ab5632e56c24e453359
MarianoRyser <rysermariano@gmail.com>
2025-03-29 14:54:47 +0100

    listo deleted profile

--- pages/profile.js
+++
@@ -1,8 +0,0 @@
-// pages/profile.js
-export async function getServerSideProps(context) {
-    const token = context.req.cookies.token; // Usar cookies es más seguro
-    if (!token) {
-      return { redirect: { destination: '/login' } };
-    }
-    // ... verificar token con tu API si es necesario
-  }
